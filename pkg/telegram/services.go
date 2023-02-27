598d1e8b8af5d0714ccb7e8620aaf1867719e8f5
Gibalov Kirill <gibalovfirecode@gmail.com>
2023-02-27 19:29:52 +0300

    Небольшой рефакторинг

--- pkg/telegram/services.go
+++ pkg/telegram/services.go
@@ -15,15 +15,12 @@ type Service struct {
 
 func (c *Service) createCategory(client *Bot, update tgbotapi.Update, updates tgbotapi.UpdatesChannel) {
 	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Вы выбрали добавить категорию. Введите название: ")
-	msg.ReplyToMessageID = update.CallbackQuery.Message.MessageID
 	if _, err := client.bot.Send(msg); err != nil {
 		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
 
-	// Ожидаем ответа пользователя
 	update = <-updates
 	if update.Message != nil {
-		// Обрабатываем ответ пользователя
 		owner := update.Message.Chat.UserName + update.Message.Chat.FirstName + update.Message.Chat.LastName
 		category := &models.CategoryName{Name: update.Message.Text, Owner: owner}
 		c.db.Create(&category)
@@ -36,16 +33,10 @@ func (c *Service) createCategory(client *Bot, update tgbotapi.Update, updates tg
 
 func (c *Service) deleteCategory(client *Bot, update tgbotapi.Update, updates tgbotapi.UpdatesChannel) {
 	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Выберите категорию:")
-	var categories []models.CategoryName
-	ownerQuery := update.CallbackQuery.From.UserName + update.CallbackQuery.From.FirstName + update.CallbackQuery.From.LastName
-	c.db.Where("owner = ?", ownerQuery).Find(&categories)
-
 	buttons := c.getCategoriesButtons(update)
-
 	categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
-
 	msg.ReplyMarkup = categoriesKeyboard
-	msg.ReplyToMessageID = update.CallbackQuery.Message.MessageID
+
 	if _, err := client.bot.Send(msg); err != nil {
 		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
@@ -60,7 +51,6 @@ func (c *Service) deleteCategory(client *Bot, update tgbotapi.Update, updates tg
 			return
 		}
 	}
-	//var toDelete models.CategoryName
 	c.db.Where("id = ?", CategoryID).Delete(&models.CategoryName{})
 	resMsg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Вы успешно удалили категорию")
 	if _, err := client.bot.Send(resMsg); err != nil {
@@ -70,16 +60,12 @@ func (c *Service) deleteCategory(client *Bot, update tgbotapi.Update, updates tg
 
 func (c *Service) createExpenses(client *Bot, update tgbotapi.Update, updates tgbotapi.UpdatesChannel) {
 	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Выберите категорию:")
-	var categories []models.CategoryName
-	ownerQuery := update.CallbackQuery.From.UserName + update.CallbackQuery.From.FirstName + update.CallbackQuery.From.LastName
-	c.db.Where("owner = ?", ownerQuery).Find(&categories)
 
 	buttons := c.getCategoriesButtons(update)
 
 	categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
 
 	msg.ReplyMarkup = categoriesKeyboard
-	msg.ReplyToMessageID = update.CallbackQuery.Message.MessageID
 	if _, err := client.bot.Send(msg); err != nil {
 		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
@@ -99,7 +85,6 @@ func (c *Service) createExpenses(client *Bot, update tgbotapi.Update, updates tg
 	}
 
 	amountMsg := tgbotapi.NewMessage(category.CallbackQuery.Message.Chat.ID, fmt.Sprintf("Введите сумму расхода:"))
-	amountMsg.ReplyToMessageID = update.CallbackQuery.Message.MessageID
 	if _, err := client.bot.Send(amountMsg); err != nil {
 		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
@@ -116,99 +101,45 @@ func (c *Service) createExpenses(client *Bot, update tgbotapi.Update, updates tg
 
 func (c *Service) getExpenses(client *Bot, update tgbotapi.Update) {
 
-	type ExpenseWithCategoryName struct {
-		Amount float64
-		Name   string
-	}
+	buttons := c.getExpensesButtons(update)
+	categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
 
-	var expenses []ExpenseWithCategoryName
-
-	if update.CallbackQuery != nil {
-		ownerQuery := update.CallbackQuery.From.UserName + update.CallbackQuery.From.FirstName + update.CallbackQuery.From.LastName
-		c.db.Model(&models.Expenses{}).
-			Select("expenses.amount, category_names.name").
-			Joins("left join category_names on category_names.id = expenses.category_id").
-			Where("category_names.owner = ?", ownerQuery).
-			Find(&expenses)
-
-		var buttons [][]tgbotapi.InlineKeyboardButton
-		if len(expenses) == 0 {
-			button := tgbotapi.NewInlineKeyboardButtonData("Расходы отсутствуют", "Ваши расходы")
-			row := []tgbotapi.InlineKeyboardButton{button}
-			buttons = append(buttons, row)
-		}
-		for _, consumption := range expenses {
-			button := tgbotapi.NewInlineKeyboardButtonData(fmt.Sprintf("Категория: %v | Количество: %v", consumption.Name, consumption.Amount), "Ваши расходы")
-			row := []tgbotapi.InlineKeyboardButton{button}
-			buttons = append(buttons, row)
-		}
-
-		categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
-
-		msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Ваши расходы")
-		msg.ReplyMarkup = categoriesKeyboard
-		if _, err := client.bot.Send(msg); err != nil {
-			log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
-		}
+	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Ваши расходы")
+	msg.ReplyMarkup = categoriesKeyboard
+	if _, err := client.bot.Send(msg); err != nil {
+		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
 }
 
 func (c *Service) deleteExpense(client *Bot, update tgbotapi.Update, updates tgbotapi.UpdatesChannel) {
 
-	type ExpenseWithCategoryName struct {
-		Id     uint
-		Amount float64
-		Name   string
+	buttons := c.getExpensesButtons(update)
+
+	categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
+
+	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Какой расход удалить")
+	msg.ReplyMarkup = categoriesKeyboard
+	if _, err := client.bot.Send(msg); err != nil {
+		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
+	expenses := <-updates
+	var expenseID int
+	var expenseError error
 
-	var expenses []ExpenseWithCategoryName
-
-	if update.CallbackQuery != nil {
-		ownerQuery := update.CallbackQuery.From.UserName + update.CallbackQuery.From.FirstName + update.CallbackQuery.From.LastName
-		c.db.Model(&models.Expenses{}).
-			Select("expenses.id, expenses.amount, category_names.name").
-			Joins("left join category_names on category_names.id = expenses.category_id").
-			Where("category_names.owner = ?", ownerQuery).
-			Find(&expenses)
-
-		var buttons [][]tgbotapi.InlineKeyboardButton
-		if len(expenses) == 0 {
-			button := tgbotapi.NewInlineKeyboardButtonData("Расходы отсутствуют", "Ваши расходы")
-			row := []tgbotapi.InlineKeyboardButton{button}
-			buttons = append(buttons, row)
-		}
-		for _, consumption := range expenses {
-			button := tgbotapi.NewInlineKeyboardButtonData(fmt.Sprintf("Категория: %v | Количество: %v", consumption.Name, consumption.Amount), fmt.Sprintf("%v", consumption.Id))
-			row := []tgbotapi.InlineKeyboardButton{button}
-			buttons = append(buttons, row)
-		}
-
-		categoriesKeyboard := tgbotapi.NewInlineKeyboardMarkup(buttons...)
-
-		msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Какой расход удалить")
-		msg.ReplyMarkup = categoriesKeyboard
-		if _, err := client.bot.Send(msg); err != nil {
-			log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
-		}
-		expenses := <-updates
-		var expenseID int
-		var expenseError error
-
-		if expenses.CallbackQuery != nil {
-			expenseID, expenseError = strconv.Atoi(expenses.CallbackQuery.Data)
-			if expenseError != nil {
-				c.cancelAction(client, update)
-				return
-			}
-		} else {
+	if expenses.CallbackQuery != nil {
+		expenseID, expenseError = strconv.Atoi(expenses.CallbackQuery.Data)
+		if expenseError != nil {
 			c.cancelAction(client, update)
 			return
 		}
-		c.db.Where("id = ?", expenseID).Delete(&models.Expenses{})
-		resMsg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Вы успешно удалили расход")
-		if _, err := client.bot.Send(resMsg); err != nil {
-			log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
-		}
+	} else {
+		c.cancelAction(client, update)
+		return
+	}
+	c.db.Where("id = ?", expenseID).Delete(&models.Expenses{})
+	resMsg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Вы успешно удалили расход")
+	if _, err := client.bot.Send(resMsg); err != nil {
+		log.Fatalf("Ошибка отправки сообщения. Текст ошибки: %s", err)
 	}
 }
 
@@ -242,6 +173,38 @@ func (c *Service) getCategoriesButtons(update tgbotapi.Update) [][]tgbotapi.Inli
 		backButton := tgbotapi.NewInlineKeyboardButtonData("Назад", "mainPage")
 		row := []tgbotapi.InlineKeyboardButton{backButton}
 		buttons = append(buttons, row)
+	}
+	return buttons
+}
+
+func (c *Service) getExpensesButtons(update tgbotapi.Update) [][]tgbotapi.InlineKeyboardButton {
+	type ExpenseWithCategoryName struct {
+		Id     uint
+		Amount float64
+		Name   string
+	}
+
+	var expenses []ExpenseWithCategoryName
+	var buttons [][]tgbotapi.InlineKeyboardButton
+
+	if update.CallbackQuery != nil {
+		ownerQuery := update.CallbackQuery.From.UserName + update.CallbackQuery.From.FirstName + update.CallbackQuery.From.LastName
+		c.db.Model(&models.Expenses{}).
+			Select("expenses.id, expenses.amount, category_names.name").
+			Joins("left join category_names on category_names.id = expenses.category_id").
+			Where("category_names.owner = ?", ownerQuery).
+			Find(&expenses)
+
+		if len(expenses) == 0 {
+			button := tgbotapi.NewInlineKeyboardButtonData("Расходы отсутствуют", "Ваши расходы")
+			row := []tgbotapi.InlineKeyboardButton{button}
+			buttons = append(buttons, row)
+		}
+		for _, consumption := range expenses {
+			button := tgbotapi.NewInlineKeyboardButtonData(fmt.Sprintf("Категория: %v | Количество: %v", consumption.Name, consumption.Amount), fmt.Sprintf("%v", consumption.Id))
+			row := []tgbotapi.InlineKeyboardButton{button}
+			buttons = append(buttons, row)
+		}
 	}
 	return buttons
 }
